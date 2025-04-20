06eaaead4afabf5248f171f2a64fe6c97af27d03
OkanDincer <dincerokan29+git@gmail.com>
2025-04-20 13:16:12 -0500

    Started grid development.

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -1,5 +1,32 @@
 package hw4.maze;
 
-public class Grid {
+import java.util.Random;
 
+public class Grid {
+	
+	private int size;
+	private Cell[][] cells;
+	
+	
+	
+	Grid(){
+		generateRand(this.size, 3, 7);
+		cells = new Cell[size][size];
+		int exitCell = 0; generateRand(exitCell, 0, size);
+		
+		
+		
+		
+	}
+	
+	private void generateRand(int size, int min, int max) {
+		Random random = new Random();
+		size = random.nextInt(max - min + 1) + min;
+		return;
+	}
+	
+	
+	
+	
+	
 }
